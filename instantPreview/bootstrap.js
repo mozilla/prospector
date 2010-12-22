4af4b8a64fdef4d593834a8031b609b037ddcc6b
Erik Vold <erikvvold@gmail.com>
2010-12-21 20:05:27 -0800

    separating common utils from code unique to instantPreview

--- instantPreview/bootstrap.js
+++ instantPreview/bootstrap.js
@@ -37,6 +37,7 @@
  * ***** END LICENSE BLOCK ***** */
 
 const {interfaces: Ci, utils: Cu} = Components;
+const global = this;
 Cu.import("resource://gre/modules/AddonManager.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
 
@@ -227,119 +228,10 @@ function addPreviews(window) {
 }
 
 /**
- * Apply a callback to each open and new browser windows.
- *
- * @usage watchWindows(callback): Apply a callback to each browser window.
- * @param [function] callback: 1-parameter function that gets a browser window.
- */
-function watchWindows(callback) {
-  // Wrap the callback in a function that ignores failures
-  function watcher(window) {
-    try {
-      callback(window);
-    }
-    catch(ex) {}
-  }
-
-  // Wait for the window to finish loading before running the callback
-  function runOnLoad(window) {
-    // Listen for one load event before checking the window type
-    window.addEventListener("load", function() {
-      window.removeEventListener("load", arguments.callee, false);
-
-      // Now that the window has loaded, only handle browser windows
-      let doc = window.document.documentElement;
-      if (doc.getAttribute("windowtype") == "navigator:browser")
-        watcher(window);
-    }, false);
-  }
-
-  // Add functionality to existing windows
-  let browserWindows = Services.wm.getEnumerator("navigator:browser");
-  while (browserWindows.hasMoreElements()) {
-    // Only run the watcher immediately if the browser is completely loaded
-    let browserWindow = browserWindows.getNext();
-    if (browserWindow.document.readyState == "complete")
-      watcher(browserWindow);
-    // Wait for the window to load before continuing
-    else
-      runOnLoad(browserWindow);
-  }
-
-  // Watch for new browser windows opening then wait for it to load
-  function windowWatcher(subject, topic) {
-    if (topic == "domwindowopened")
-      runOnLoad(subject);
-  }
-  Services.ww.registerNotification(windowWatcher);
-
-  // Make sure to stop watching for windows if we're unloading
-  unload(function() Services.ww.unregisterNotification(windowWatcher));
-}
-
-/**
- * Save callbacks to run when unloading. Optionally scope the callback to a
- * container, e.g., window. Provide a way to run all the callbacks.
- *
- * @usage unload(): Run all callbacks and release them.
- *
- * @usage unload(callback): Add a callback to run on unload.
- * @param [function] callback: 0-parameter function to call on unload.
- * @return [function]: A 0-parameter function that undoes adding the callback.
- *
- * @usage unload(callback, container) Add a scoped callback to run on unload.
- * @param [function] callback: 0-parameter function to call on unload.
- * @param [node] container: Remove the callback when this container unloads.
- * @return [function]: A 0-parameter function that undoes adding the callback.
- */
-function unload(callback, container) {
-  // Initialize the array of unloaders on the first usage
-  let unloaders = unload.unloaders;
-  if (unloaders == null)
-    unloaders = unload.unloaders = [];
-
-  // Calling with no arguments runs all the unloader callbacks
-  if (callback == null) {
-    unloaders.slice().forEach(function(unloader) unloader());
-    unloaders.length = 0;
-    return;
-  }
-
-  // The callback is bound to the lifetime of the container if we have one
-  if (container != null) {
-    // Remove the unloader when the container unloads
-    container.addEventListener("unload", removeUnloader, false);
-
-    // Wrap the callback to additionally remove the unload listener
-    let origCallback = callback;
-    callback = function() {
-      container.removeEventListener("unload", removeUnloader, false);
-      origCallback();
-    }
-  }
-
-  // Wrap the callback in a function that ignores failures
-  function unloader() {
-    try {
-      callback();
-    }
-    catch(ex) {}
-  }
-  unloaders.push(unloader);
-
-  // Provide a way to remove the unloader
-  function removeUnloader() {
-    let index = unloaders.indexOf(unloader);
-    if (index != -1)
-      unloaders.splice(index, 1);
-  }
-  return removeUnloader;
-}
-
-/**
  * Handle the add-on being activated on install/enable
  */
 function startup(data, reason) AddonManager.getAddonByID(data.id, function(addon) {
+  Services.scriptloader.loadSubScript(Services.io.newFileURI(data.installPath).spec + "includes/utils.js", global);
   Cu.import("resource://services-sync/util.js");
   watchWindows(addPreviews);
 });
@@ -351,14 +243,6 @@ function shutdown(data, reason) {
   // Clean up with unloaders when we're deactivating
   if (reason != APP_SHUTDOWN)
     unload();
-}
-
-/**
- * Helper that adds event listeners and remembers to remove on unload
- */
-function listen(window, node, event, func) {
-  node.addEventListener(event, func, true);
-  unload(function() node.removeEventListener(event, func, true), window);
 }
 
 function install() {}
