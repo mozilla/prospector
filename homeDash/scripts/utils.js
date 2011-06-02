43576feff91ae27b351ddd62bb79a4bb26b4ad1d
Edward Lee <edilee@mozilla.com>
2011-06-02 14:46:07 -0700

    Closes #484: Utils.queryAsync doesn't exist on Nightly Add a spinQuery util function to all the add-ons.

--- homeDash/scripts/utils.js
+++ homeDash/scripts/utils.js
@@ -201,6 +201,94 @@ function makeTrigger() {
 }
 
 /**
+ * Synchronously query with an async statement fetching results by name
+ */
+function spinQuery(connection, {names, params, query}) {
+  // Initialize the observer to watch for application quits during a query
+  if (spinQuery.checkReady == null) {
+    // In the common case, return true to continue execution
+    spinQuery.checkReady = function() true;
+
+    // Change the checkReady function to throw to abort
+    let abort = function(reason) {
+      spinQuery.checkReady = function() {
+        throw reason;
+      };
+    };
+
+    // Add the observer and make sure to clean up
+    let onQuit = function() abort("Application Quitting");
+    Services.obs.addObserver(onQuit, "quit-application", false);
+    unload(function() Services.obs.removeObserver(onQuit, "quit-application"));
+
+    // Also watch for unloads to stop queries
+    unload(function() abort("Add-on Unloading"));
+  }
+
+  // Remember the results from processing the query
+  let allResults = [];
+  let status;
+
+  // Nothing to do with no query
+  if (query == null)
+    return allResults;
+
+  // Create the statement and add parameters if necessary
+  let statement = connection.createAsyncStatement(query);
+  if (params != null) {
+    Object.keys(params).forEach(function(key) {
+      statement.params[key] = params[key];
+    });
+  }
+
+  // Start the query and prepare to cancel if necessary
+  let pending = statement.executeAsync({
+    // Remember that we finished successfully
+    handleCompletion: function handleCompletion(reason) {
+      if (reason != Ci.mozIStorageStatementCallback.REASON_ERROR)
+        status = allResults;
+    },
+
+    // Remember that we finished with an error
+    handleError: function handleError(error) {
+      status = error;
+    },
+
+    // Process the batch of results and save them for later
+    handleResult: function handleResult(results) {
+      let row;
+      while ((row = results.getNextRow()) != null) {
+        let item = {};
+        names.forEach(function(name) {
+          item[name] = row.getResultByName(name);
+        });
+        allResults.push(item);
+      }
+    },
+  });
+
+  // Grab the current thread so we can make it give up priority
+  let thread = Cc["@mozilla.org/thread-manager;1"].getService().currentThread;
+
+  // Keep waiting until the query finished unless aborting
+  try {
+    while (spinQuery.checkReady() && status == null)
+      thread.processNextEvent(true);
+  }
+  // Must be aborting, so cancel the query
+  catch(ex) {
+    pending.cancel();
+    status = ex;
+  }
+
+  // Must have completed with an error so expose it
+  if (status != allResults)
+    throw status;
+
+  return allResults;
+}
+
+/**
  * Save callbacks to run when unloading. Optionally scope the callback to a
  * container, e.g., window. Provide a way to run all the callbacks.
  *
