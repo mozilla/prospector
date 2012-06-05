418a450d5d7b68305b049574ac50400949771810
Maxim Zhilyaev <mzhilyaev@mozilla.com>
2012-06-05 11:00:32 -0700

    adding newnewtab add on to the repo

---
+++ siteSuggest/lib/appViewer.js
@@ -0,0 +1,224 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is NewTab.
+ *
+ * The Initial Developer of the Original Code is The Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2012
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Edward Lee <edilee@mozilla.com>
+ *   Maxim Zhilayev <mzhilyaev@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+"use strict";
+const {Ci,Cu} = require("chrome");
+const {makeWindowHelpers} = require("makeWindowHelpers");
+const {unload} = require("unload+");
+const {watchWindows} = require("watchWindows");
+const {trace} = require("dumper");
+const {listen} = require("listen");
+const {data} = require("self");
+const file = require("file");
+const {XMLHttpRequest} = require("xhr");
+const timers = require("timers");
+const simplePrefs = require("simple-prefs");
+
+Cu.import("resource://gre/modules/Services.jsm", this);
+const XHTML_NS = "http://www.w3.org/1999/xhtml";
+const CATEGORY_RESULTS = {
+Arts: 0.9,
+Business: 0.8,
+Computers: 0.6,
+Games: 0.7,
+Health: 0.8,
+Home: 0.9,
+News: 0.7,
+Recreation: 0.2,
+Reference: 0.1,
+Regional: 0.2,
+Science: 0.1,
+Shopping: 0.2,
+Society: 0.1
+};
+
+const DEMOGRAPHICS_RESULTS = {
+age_18: 0.2,
+age_25: 0.3,
+age_35: 0.7,
+age_45: 0.9,
+age_55: 0.3,
+age_65: 0.1,
+no_college: 0.1,
+some_college: 0.3,
+college: 0.1,
+graduate: 0.4,
+male: 0.1,
+female: 0.5,
+children: 0.5,
+no_children: 0.1,
+home: 0.2,
+school: 0.1,
+work: 0.8
+}
+
+function AppViewer( configObject ) {
+try{
+    this._window = configObject.window;
+	this._document = configObject.document;
+	this._backGroundElement = configObject.bElement;
+    //let {change, createNode, listen, unload} = makeWindowHelpers(this._window);
+	let div  = this._document.getElementById( "newtab-vertical-margin");
+
+	let iframe = this._document.createElementNS(XHTML_NS, "iframe");
+	iframe.setAttribute( "type" , "content" );
+    iframe.style.left = "-1000px";
+    iframe.style.top = "36px";
+    iframe.style.width = "85%";
+    iframe.style.height = "90%";
+	iframe.style.position = "absolute";
+	iframe.style.background = "white";
+
+    iframe.style.boxShadow = "5px 5px 10px black";
+    iframe.style.borderRadius = "10px 10px 10px 10px";
+
+	//iframe.style.MozTransitionProperty = "left";
+	//iframe.style.MozTransitionDuration = "2s";
+
+	// remove the border pixels
+	iframe.style.borderWidth = "0px";
+	iframe.style.borderLeftWidth = "0px";
+	iframe.style.borderRightWidth = "0px";
+	iframe.style.borderTopWidth = "0px";
+	iframe.style.borderBottomWidth = "0px";
+
+	// now that we have iframe, let's install apis into it
+    var apiInjector = function(doc, topic, data) {
+	try {
+		// make sure that it's our iframe document that has been inserted into iframe
+		if( !doc.defaultView || doc.defaultView != iframe.contentWindow) {
+		          return;  // so it was not
+	    }
+		console.log( "caught document insertion" );
+		Services.obs.removeObserver(apiInjector, 'document-element-inserted', false);
+
+		iframe.contentWindow.wrappedJSObject.getCategories = function( callback ) {   
+			callback( CATEGORY_RESULTS );
+		}
+
+		iframe.contentWindow.wrappedJSObject.getDemographics = function( callback ) {   
+			callback( DEMOGRAPHICS_RESULTS );
+		}
+	  } catch (ex) {  console.log( "ERROR " + ex ); }
+	};
+	Services.obs.addObserver( apiInjector , 'document-element-inserted', false);
+
+	//iframe.src = "https://myapps.mozillalabs.com";
+	iframe.src = (simplePrefs.prefs.apps_page_url || data.url( "newtab_test.html" ));
+	//iframe.src = data.url( "newtab_test.html" );
+	// insert doc into the thing
+	div.parentNode.insertBefore(iframe, div.nextSibling)
+
+	// move left to clientWidth
+	iframe.style.left =  "-" + iframe.clientWidth + "px";
+	this._iframe = iframe;
+	this._shown = false;
+
+	var self = this;
+    iframe.onload = function ( event ) {
+		console.log( "loaded" );
+		iframe.contentWindow.addEventListener("click", function( event ) {
+	        console.log("iframe clicked");
+			if( self._shown == false ) {
+				self.show( );
+			}
+	   });
+	   self.hide( );
+	};
+
+	iframe.contentWindow.onresize = function ( event ) {
+		self.resize( );
+	};
+
+    /*** this is no loger needed, since we have buttons coltrooling hide/show 
+	//
+	this._backGroundElement.addEventListener("click", function(event) {
+	        console.log( "CLICKED DIV" );
+			if( self._shown == true ) {
+				self.hide( );
+			}
+      } );
+	 ****/
+
+} catch ( ex ) {
+
+    console.log( "ERROR" + ex );
+
+}
+}
+
+
+
+AppViewer.prototype = {
+    show: function () {
+	try {
+		let baseWidth = this._backGroundElement.clientWidth;
+		let leftExtent = ( baseWidth - this._iframe.clientWidth ) / 2;
+		this._shown = true;
+		//this._iframe.style.visibility = "visible";
+		this._iframe.style.MozTransitionProperty = "left";
+		this._iframe.style.MozTransitionDuration = "1s";
+		this._iframe.style.left = leftExtent + "px";
+		this._backGroundElement.style.opacity = "0.5";
+		} catch( ex ) { console.log( "ERROR" + ex ); }
+    } ,
+
+    hide: function () {
+	try {
+		//let baseWidth = this._backGroundElement.clientWidth;
+		let leftExtent = this._iframe.clientWidth + 10;
+		this._iframe.style.left = "-" + leftExtent + "px";
+
+		this._window.setTimeout(  function( ) {
+			this._iframe.style.MozTransitionProperty = "";
+			this._iframe.style.MozTransitionDuration = "";
+			this._backGroundElement.style.opacity = "";
+			//this._iframe.style.visibility = "hidden";
+			this._shown = false;
+		}.bind( this ) , 1000 );
+		} catch( ex ) { console.log( "ERROR" + ex ); }
+    } ,
+
+	resize: function( ) {
+	    console.log( "in resize" );
+		if( this._shown == true ) return;
+		let leftExtent = this._iframe.clientWidth - 40;
+        this._iframe.style.left = "-" + leftExtent + "px";
+	},
+}
+
+exports.AppViewer = AppViewer;
---
+++ siteSuggest/lib/main.js
@@ -0,0 +1,135 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Initial Developer of the Original Code is The Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2012
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Edward Lee <edilee@mozilla.com>
+ *   Maxim Zhilayev <mzhilyaev@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+"use strict";
+const {Ci,Cu,Cc} = require("chrome");
+const tabs = require("tabs");
+const {makeWindowHelpers} = require("makeWindowHelpers");
+const {unload} = require("unload+");
+const {watchWindows} = require("watchWindows");
+const {AppViewer} = require("appViewer");
+const {PageMod} = require("page-mod");
+const {data} = require("self");
+const {listen} = require("listen");
+const simplePrefs = require("simple-prefs");
+const file = require("file");
+const {XMLHttpRequest} = require("xhr");
+const timers = require("timers");
+
+Cu.import("resource://gre/modules/Services.jsm", this);
+const PromptService = Cc["@mozilla.org/embedcomp/prompt-service;1"].getService(Components.interfaces.nsIPromptService);
+const ObserverService = require("observer-service");
+
+var index = 1;
+
+function addAppsButton( window , browser ) {
+
+  let document = browser.contentDocument;
+  if( !document ) return; // sanity
+  let hisToggle = document.getElementById( "newtab-toggle");
+  if( ! hisToggle ) return;   // sanity
+
+
+  let div = document.getElementById( "newtab-vertical-margin");
+
+//  listen( window , div , "click" , function(event) {
+//			console.log( "CLICKED DIV" );
+//     } );
+
+  let contentWindow = browser.contentWindow;
+  let appToggle = hisToggle.cloneNode(true);
+  appToggle.setAttribute("id", "apps-toggle");
+  appToggle.style.position = "absolute";
+  appToggle.style.width = "16px";
+  appToggle.style.height = "16px";
+  appToggle.style.height = "16px";
+  appToggle.style.top = "12px";
+  appToggle.style.right = "40px";
+  hisToggle.parentNode.insertBefore(appToggle, hisToggle.nextSibling);
+  var toggleStateShown = false;
+  var appViewer = new AppViewer( { 
+  					window: window,
+					document: document,
+					bElement: div,
+					});
+
+   contentWindow.onresize = function onRes(event) {
+   		appViewer.resize( );
+   };
+
+  appToggle.onclick = function( ) { 
+    
+  	if( toggleStateShown ) { 
+		appViewer.hide( );
+		toggleStateShown = false;
+	} else {
+		appViewer.show( );
+		toggleStateShown = true;
+   }
+  };
+
+  var oldHandler = hisToggle.onclick;
+  hisToggle.onclick = function( ) {
+  	appViewer.hide( );
+    toggleStateShown = false;
+	oldHandler( );
+  }
+
+}
+
+exports.main = function(options) {
+try {
+
+  // per-window initialization
+  watchWindows(function(window) {
+    // let {change, createNode, listen, unload} = makeWindowHelpers(window);
+    let {gBrowser} = window;
+
+   // Listen for tab content loads.
+	tabs.on('ready', function(tab) {
+		if( tabs.activeTab.url == "about:newtab" ) {
+			addAppsButton( window , gBrowser );
+		}
+	});
+
+  });
+
+}
+catch ( ex ) {
+
+	console.log( "ERROR" + ex );
+
+}
+}
