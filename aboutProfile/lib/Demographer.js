54f0946b8cfda2f9092739abc8fdb50d67b3737e
Maxim Zhilyaev <mzhilyaev@mozilla.com>
2012-08-01 11:51:27 -0700

    ported newnewtab changes to Demographer to speed up addon

--- aboutProfile/lib/Demographer.js
+++ aboutProfile/lib/Demographer.js
@@ -1,47 +1,11 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Initial Developer of the Original Code is the Mozilla Foundation.
- * Portions created by the Initial Developer are Copyright (C) 2011 the
- * Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 'use strict';
 
 const file = require("file");
-const widgets = require("widget");
-const tabs = require("tabs");
-const request = require("request");
 const timers = require("timers");
-const windows = require("windows");
-const simpleStorage = require("simple-storage");
-const preferences = require("preferences-service");
-const {PageMod} = require("page-mod");
 const {data} = require("self");
 const passwords = require("passwords");
 
@@ -49,8 +13,6 @@ const {Cc,Ci,Cm,Cr,Cu,components} = require("chrome");
 
 Cm.QueryInterface(Ci.nsIComponentRegistrar);
 
-Cu.import("resource://gre/modules/PlacesUtils.jsm", this);
-Cu.import("resource://gre/modules/XPCOMUtils.jsm", this);
 Cu.import("resource://gre/modules/Services.jsm", this);
 
 const historyUtils = require("HistoryUtils");
@@ -75,18 +37,6 @@ const demogBuckets = [
 "work"
 ];
 
-const WeightFunctionTags = [
-
-"total_visists",
-"log_total_visists",
-"daily_visists",
-"log_daily_visists",
-"time_reverse_sum",
-"time_reverse_log_sum"
-
-];
-
-
 function Demographer( sitesCatFile , sitesDemogFile  ) {
     this.catFile = sitesCatFile;
     this.demogFile = sitesDemogFile;
@@ -98,33 +48,8 @@ function Demographer( sitesCatFile , sitesDemogFile  ) {
 		this.demog[ demogBuckets[x] ] = { vtotal: 0 , neg: new ItemJar(5) , pos: new ItemJar(5) };
 	}
 
-	this.weighter = {
-		total_visists: function( stats ) { return stats.total; },
-		log_total_visists: function( stats ) { return Math.log(stats.total); },
-		total_day_visists: function( stats ) { return (stats.dtotal); },
-		log_total_day_visists: function( stats ) { return Math.log(stats.dtotal); },
-		reverse_age_sum: function( stats ) { return stats.daysOverSum; },
-		reverse_log_age_sum: function( stats ) { return stats.daysOverLogSum; },
-		reverse_age_day_sum: function( stats ) { return stats.dOverSum; },
-		reverse_log_age_day_sum: function( stats ) { return stats.dOverLogSum; } ,
-		frecency_max: function( stats ) { return stats.frecencyMax; } ,
-		frecency_sum: function( stats ) { return stats.frecencySum; } ,
-		frecency_avg: function( stats ) { return stats.frecencySum * 1.0 / stats.total ; } ,
-		log_frecency_max: function( stats ) { return Math.log(stats.frecencyMax); } ,
-		log_frecency_sum: function( stats ) { return Math.log(stats.frecencySum); } ,
-		log_frecency_avg: function( stats ) { return Math.log(stats.frecencySum * 1.0 / stats.total) ; }
-	};
-
-	this.currentWeighter = "log_total_visists";
-	this.totalLimit = 1;
 	this.catDepth = 2;
-	this.frecencyLimit = 0;
-	this.dayLimit = 300;
-
 	this.totalAcross = 0;
-	this.totalSquared = 0;
-
-    this.misses = new ItemJar(20);
 
 	this.readCats( );
 	this.readDemographics( );
@@ -140,9 +65,7 @@ Demographer.prototype = {
     for ( x in demogBuckets ) {
 	        this.demog[ demogBuckets[x] ] = { vtotal: 0 , neg: new ItemJar(5) , pos: new ItemJar(5) };
     }
-	this.misses = new ItemJar(20);
 	this.totalAcross = 0;
-	this.totalSquared = 0;
   },
 
   rebuild: function ( cb ) {
@@ -152,101 +75,62 @@ Demographer.prototype = {
 	//this.computeSitesData( );
   },
 
-  setDayLimit: function (val) { this.dayLimit = val; } ,
-  getDayLimit: function () { return this.dayLimit; } ,
-  setTotalLimit: function (val) { this.totalLimit = val; } ,
-  getTotalLimit: function () { return this.totalLimit; } ,
-  setCatDepth: function (val) { this.catDepth = val; } ,
-  getCatDepth: function () { return this.catDepth; } ,
-  setFrecencyLimit: function (val) { this.frecencyLimit = val; } ,
-  getFrecencyLimit: function () { return this.frecencyLimit; } ,
-
-  getWeightFunctions: function ( ) { return Object.keys( this.weighter ); } ,
-  getCurrentWeightFunction: function( ) { return this.currentWeighter; } ,
-  setCurrentWeightFunction: function( val) { this.currentWeighter = val; } ,
-
-  getMissingSites: function( ) { return this.misses; } ,
   getTotalAcross: function( ) { return this.totalAcross; } ,
-  getTotalDist: function( ) { return Math.sqrt(this.totalSquared); } ,
 
-  extractDomain: function ( url ) {
+  extractDomain: function ( domain ) {
+    // and make sure to get rid of www
+    let re = /^www[.]/;
+    domain = domain.replace(re, "");
 
-    // now we need to grep the domain 
-      let domain = require("url").URL( url ).host;
+    // ok check if site is present in our global site list
+    let siteData = this.allSites[domain];
 
-      // and make sure to get rid of www
-      let re = /^www[.]/;
-      domain = domain.replace( re , "" );
-
-      // ok check if site is present in our global site list
-      let siteData = this.allSites[ domain ];
-
-      while( ! siteData ) {  // attempt to go to the root domain
-           domain = domain.replace( /^[^.]+[.]/ , "" );
-           //console.log( "cutting to " + domain );
-           if( domain.indexOf( "." ) <  0) {
-               domain  = null;
-               break;
-           }  // no need to go further
-           siteData = this.allSites[ domain ];
+    // attempt to go to the root domain, keep the lastDomain
+    // so that we never ran into endless loop if regex does not replace
+    // anything.  Hence, regex failes on strings starting with '.'
+    let lastDomain = domain;
+    while (!siteData) {
+      domain = domain.replace(/^[^.]+[.]/, "");
+      if (domain == lastDomain || domain.length <= 1 || domain.indexOf(".") < 0) {
+        domain = null;
+        // no need to go further
+        break;
       }
+      siteData = this.allSites[domain];
+    }
 
-	  return ( siteData ) ? domain : null;
+    return siteData ? domain : null;
   },
 
   readHistory: function ( cb ) {
 
-	//query = "select visit_count , url from moz_places where visit_count >= 1";
-	console.log( this.totalLimit , this.frecencyLimit , this.dayLimit );
-	let query  = "select (strftime( '%s' , 'now') - visit_date/1000000) / (60*60*24) as day_diff, " +
-	             "strftime('%s' , visit_date/1000000  ,'unixepoch' ) / (60*60*24) as day , " +
-				 "date( visit_date/1000000 , 'unixepoch' ) as date ," +
-				 "url, " +
-				 "frecency " +
-				 "from moz_historyvisits , moz_places where moz_historyvisits.place_id = moz_places.id " + 
-				 "and moz_places.frecency > " + this.frecencyLimit + " " +
-				 "and day_diff < " + this.dayLimit + " " +
-				 "order by visit_date desc";
-
-    console.log( query );
-
+    let query = "select SUM(visit_count), rev_host from moz_places where visit_count >= 1 group by rev_host";
 	var s1 = Date.now();
     historyUtils.executeHistoryQuery( query , null , 
 	   {
 	     onRow: function ( row ) {
-		   let daysPassed = row.getResultByIndex( 0 );
-		   let onDay = row.getResultByIndex( 1 );
-		   let date = row.getResultByIndex( 2 );
-           let url = row.getResultByIndex( 3 );
-           let frecency = row.getResultByIndex( 4 );
+          let vcount = row.getResultByIndex(0);
+          let rev_host = row.getResultByIndex(1);
+          let host = rev_host.split("").reverse().join("");
 
-		   // now we need to grep the domain 
-		   let domain = this.extractDomain( url );
-		   if( ! domain ) return;   // bail if domain is empty
+          // if host is preceeded with '.', remove it
+          if (host.charAt(0) == '.') {
+            host = host.slice(1);
+          }
 
- 			let site = this.mySites[ domain ];
-			if( !this.mySites[ domain ] ) {
-				site = { total: 0 , frecencySum: 0 , 
-				         frecencyMax: 0, daysOverSum: 0, 
-				          daysOverLogSum: 0 , dtotal: 0 ,  
-						  dOverSum: 0 , dOverLogSum: 0, lsd: 0 };
-				this.mySites[ domain ]  = site;
-			}
-			site.total ++;
-			site.daysOverSum += 1.0 / (1 + daysPassed);
-			site.daysOverLogSum += 1.0 / (1 + Math.log( 1 + daysPassed));
-			site.frecencySum += frecency;
+          // now we need to grep the domain
+          let domain = this.extractDomain(host);
+          // bail if domain is empty
+          if (!domain) {
+            return;
+          }
 
-			if( frecency > site.frecencyMax ) {
-				site.frecencyMax = frecency;
-			}
+          let site = this.mySites[domain];
+          if (!this.mySites[domain]) {
+            this.mySites[domain] = 0;
+          }
+          this.mySites[domain] += vcount;
 
-			if( site.lsd != onDay ) {
-				site.lsd = onDay;
-				site.dtotal ++;
-				site.dOverSum += 1.0 / (1 + daysPassed);
-				site.dOverLogSum += 1.0 / (1 + Math.log( 1 + daysPassed));
-		    }
        	}.bind( this ) ,
 
 		onCompletion: function ( reason ) {
@@ -266,22 +150,18 @@ Demographer.prototype = {
   },
 
   computeSitesData: function( ) {
-  	//console.log( JSON.stringify( this.mySites ) );
-  	for ( domain in this.mySites ) {
-		if( domain && this.mySites[ domain ].total > this.totalLimit ) {
-			//console.log( domain , JSON.stringify( this.mySites[ domain ] ) );
+  	for ( let domain in this.mySites ) {
+		if( domain ) {
 		    this.processHistorySite( domain );
 		}
 	}
-	//console.log( "CATS " + JSON.stringify( this.cats) );
-	//console.log( "DEMOG " + JSON.stringify( this.demog) );
   },
 
   computeSiteWeight: function( domain ) {
   		
-		let data = this.mySites[ domain ];
-		if( data ) {
-			return this.weighter[ this.currentWeighter ]( data );
+		let vcount = this.mySites[ domain ];
+		if( vcount ) {
+			return Math.log(vcount);
 		}
 		else {
 			return 0;
@@ -305,18 +185,13 @@ Demographer.prototype = {
 			this.addToCategory( domain , category , vcount , addedHash );
 		}.bind( this ));
 		this.totalAcross += vcount;
-		this.totalSquared += vcount*vcount;
-	} else {
-		// no categories add to missing sites
-		this.misses.addItem( domain , vcount );
-    }
+	}
 
 	if( siteData.demog ) {
 		// so we have demographics data add them to bukets
 		for( x in demogBuckets ) {
 			let buketName = demogBuckets[x];
 			let bucketDrop  = siteData.demog[x];
-			//console.log( domain , buketName , bucketDrop );
 			this.demog[ buketName ].vtotal += vcount * bucketDrop;
 			if( bucketDrop < 0 ) {
 				//console.log( domain , vcount , bucketDrop );
@@ -409,13 +284,6 @@ Demographer.prototype = {
 
   	return this.demog;
 
-  },
-
-  normalize: function( ) {
-  	let dist = this.getTotalDist( );
-  	Object.keys( this.cats ).forEach( function ( key ) {
-		this.cats[ key ].vcount = this.cats[ key ].vcount / dist ;
-	}.bind( this ));
   },
 
 }
