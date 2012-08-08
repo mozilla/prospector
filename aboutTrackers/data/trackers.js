5bbc2aad99fb307b0870d8e1172257de162b1749
Edward Lee <edilee@mozilla.com>
2012-08-07 22:06:44 -0700

    Initial implementation of about:trackers with autoblocking connections.

---
+++ aboutTrackers/data/trackers.js
@@ -0,0 +1,68 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+// Fill in the threshold and add functionality
+self.port.on("show_threshold", function(threshold) {
+  let span = document.getElementById("threshold");
+  span.textContent = threshold;
+
+  // Update the threshold on clicks and save the value
+  [["minus", -1], ["plus", 1]].forEach(function([id, delta]) {
+    let button = document.getElementById("threshold-" + id);
+    button.addEventListener("click", function() {
+      threshold += delta;
+      span.textContent = threshold;
+      self.port.emit("set_threshold", threshold);
+    });
+  });
+});
+
+// Build a table with the trackers and blocked status
+self.port.on("show_trackers", function(trackers, blocked, cookied) {
+  let table = document.getElementById("trackers");
+  Object.keys(trackers).sort().sort(function(a, b) {
+    return Object.keys(trackers[b]).length - Object.keys(trackers[a]).length;
+  }).forEach(function(tracker) {
+    let tr = document.createElement("tr");
+    table.appendChild(tr);
+
+    let blockTd = document.createElement("td");
+    tr.appendChild(blockTd);
+
+    // Allow setting or clearing the blocked state
+    let blockCheck = document.createElement("input");
+    blockCheck.id = "check-" + tracker;
+    blockCheck.type = "checkbox";
+    blockCheck.checked = blocked[tracker];
+    blockTd.appendChild(blockCheck);
+
+    // Track the toggle to storage
+    blockTd.addEventListener("click", function() {
+      self.port.emit("toggle_block", tracker);
+    });
+
+    let trackerTd = document.createElement("td");
+    trackerTd.textContent = tracker;
+    tr.appendChild(trackerTd);
+
+    if (cookied[tracker]) {
+      tr.style.fontWeight = "bold";
+    }
+    else {
+      tr.style.fontStyle = "italic";
+    }
+
+    let tracked = Object.keys(trackers[tracker]).sort();
+    let trackedTd = document.createElement("td");
+    trackedTd.textContent = "(" + tracked.length + ") " + tracked.join(" ");
+    tr.appendChild(trackedTd);
+  });
+});
+
+// Handle changes to blocked statuses
+self.port.on("update_block", function(tracker, blocked) {
+  document.getElementById("check-" + tracker).checked = blocked;
+});
---
+++ aboutTrackers/lib/main.js
@@ -0,0 +1,193 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const {Ci, Cc, Cm, Cr, Cu} = require("chrome");
+const {Class} = require("api-utils/heritage");
+const {data} = require("self");
+const {Factory, Unknown} = require("api-utils/xpcom");
+const {PageMod} = require("page-mod");
+const privateBrowsing = require("private-browsing")
+const {setTimeout} = require("timers");
+const {storage} = require("simple-storage");
+const unload = require("unload");
+
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+exports.main = function() {
+  // Initialize persistent data
+  if (storage.autoBlockThreshold == null) {
+    storage.autoBlockThreshold = 14;
+  }
+  if (storage.blocked == null) {
+    storage.blocked = {
+      // Various sites depend on this for jQuery, etc.
+      "googleapis.com": 0,
+    };
+  }
+  if (storage.trackers == null) {
+    storage.trackers = {};
+  }
+
+  // Do some cleaning of single-site trackers to save some space
+  unload.when(function() {
+    Object.keys(storage.trackers).forEach(function(tracker) {
+      if (Object.keys(storage.trackers[tracker]).length == 1) {
+        delete storage.trackers[tracker];
+      }
+    });
+  });
+
+  // Detect and block trackers with a content policy
+  ({
+    classDescription: "about:trackers content policy",
+    classID: Components.ID("d27de1fd-f2cc-4f84-be48-65d2510123b5"),
+    contractID: "@mozilla.org/about-trackers/content-policy;1",
+    QueryInterface: XPCOMUtils.generateQI([Ci.nsIContentPolicy, Ci.nsIFactory]),
+
+    init: function() {
+      let registrar = Cm.QueryInterface(Ci.nsIComponentRegistrar);
+      registrar.registerFactory(this.classID, this.classDescription, this.contractID, this);
+
+      let catMan = Cc["@mozilla.org/categorymanager;1"].getService(Ci.nsICategoryManager);
+      catMan.addCategoryEntry("content-policy", this.contractID, this.contractID, false, true);
+
+      unload.when(function() {
+        catMan.deleteCategoryEntry("content-policy", this.contractID, false);
+
+        // This needs to run asynchronously, see bug 753687
+        Services.tm.currentThread.dispatch(function() {
+          registrar.unregisterFactory(this.classID, this);
+        }.bind(this), Ci.nsIEventTarget.DISPATCH_NORMAL);
+      }.bind(this));
+    },
+
+    shouldLoad: function(contentType, contentLocation, requestOrigin, context, mimeTypeGuess, extra) {
+      // Return to normal behavior (even not blocking) when private browsing
+      if (privateBrowsing.isActive) {
+        return Ci.nsIContentPolicy.ACCEPT;
+      }
+
+      try {
+        // Ignore top level browser document loads
+        if (contentType == Ci.nsIContentPolicy.TYPE_DOCUMENT) {
+          return Ci.nsIContentPolicy.ACCEPT;
+        }
+
+        // Ignore requests that share a base domain
+        let trackerDomain = Services.eTLD.getBaseDomain(contentLocation);
+        let topLevel = (context.ownerDocument || context).defaultView.top.location.host;
+        let contextDomain = Services.eTLD.getBaseDomainFromHost(topLevel);
+        if (trackerDomain == contextDomain) {
+          return Ci.nsIContentPolicy.ACCEPT;
+        }
+
+        // We have a 3rd-party tracker, so initialize if it's new
+        if (storage.trackers[trackerDomain] == null) {
+           storage.trackers[trackerDomain] = {};
+        }
+        if (storage.trackers[trackerDomain][contextDomain] == null) {
+          storage.trackers[trackerDomain][contextDomain] = 1;
+
+          // Auto-block if we exceed and don't have a setting
+          let tracked = Object.keys(storage.trackers[trackerDomain]).length;
+          if (tracked >= storage.autoBlockThreshold &&
+              storage.blocked[trackerDomain] == null) {
+            storage.blocked[trackerDomain] = "auto";
+          }
+        }
+
+        // Check if this tracker should be blocked (auto or manual)
+        if (storage.blocked[trackerDomain]) {
+          return Ci.nsIContentPolicy.REJECT_REQUEST;
+        }
+      }
+      catch(ex) {}
+      return Ci.nsIContentPolicy.ACCEPT;
+    },
+
+    shouldProcess: function(contentType, contentLocation, requestOrigin, context, mimeType, extra) {
+      return Ci.nsIContentPolicy.ACCEPT;
+    },
+
+    createInstance: function(outer, iid) {
+      if (outer) {
+        throw Cr.NS_ERROR_NO_AGGREGATION;
+      }
+      return this.QueryInterface(iid);
+    }
+  }).init();
+
+  // Handle about:trackers requests
+  Factory({
+    contract: "@mozilla.org/network/protocol/about;1?what=trackers",
+
+    Component: Class({
+      extends: Unknown,
+
+      interfaces: ["nsIAboutModule"],
+
+      newChannel: function(uri) {
+        let chan = Services.io.newChannel(data.url("trackers.html"), null, null);
+        chan.originalURI = uri;
+        return chan;
+      },
+
+      getURIFlags: function(uri) {
+        return 0;
+      }
+    })
+  });
+
+  // Add functionality into about:trackers page loads
+  PageMod({
+    contentScriptFile: [
+      data.url("trackers.js"),
+    ],
+
+    include: ["about:trackers"],
+
+    onAttach: function(worker) {
+      let cookied = {};
+      Object.keys(storage.trackers).forEach(function(tracker) {
+        cookied[tracker] = Services.cookies.countCookiesFromHost(tracker) > 0;
+      });
+
+      // Update the page with stored values
+      worker.port.emit("show_threshold", storage.autoBlockThreshold);
+      worker.port.emit("show_trackers", storage.trackers, storage.blocked, cookied);
+
+      // Save changes to the threshold
+      worker.port.on("set_threshold", function(threshold) {
+        storage.autoBlockThreshold = threshold;
+
+        // Auto-block/unblock sites for the new threshold
+        Object.keys(storage.trackers).forEach(function(tracker) {
+          // Ignore user-set blocked values
+          let oldBlocked = storage.blocked[tracker];
+          if (typeof oldBlocked == "number") {
+            return;
+          }
+
+          // Check the number of tracked sites against the threshold
+          let tracked = Object.keys(storage.trackers[tracker]).length;
+          storage.blocked[tracker] = tracked < threshold ? undefined : "auto";
+
+          // Update the UI for trackers that changed
+          if (oldBlocked != storage.blocked[tracker]) {
+            worker.port.emit("update_block", tracker, storage.blocked[tracker]);
+          }
+        });
+      });
+
+      // Save changes to the block status for a tracker
+      worker.port.on("toggle_block", function(tracker) {
+        storage.blocked[tracker] = +!storage.blocked[tracker];
+        worker.port.emit("update_block", tracker, storage.blocked[tracker]);
+      });
+    }
+  });
+};
